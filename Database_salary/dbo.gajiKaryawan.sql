c5a2500a980dbf388712bf5154b8867eee6d0419
muhammadnaufal02 <79163464+muhammadnaufal02@users.noreply.github.com>
2021-04-29 08:44:57 +0700

    Add files via upload

---
+++ Database_salary/dbo.gajiKaryawan.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Gaji Karyawan]
+(
+	[nama_karyawan] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [nip_karyawan] INT NOT NULL, 
+    [gajiPokok_karyawan] INT NOT NULL, 
+    [masaKerja_karyawan] INT NOT NULL
+)
